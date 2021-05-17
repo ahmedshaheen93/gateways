281ec0cb99c36bf9f6532790eac19a01064e0147
ahmed shaheen <ahmedshaheen93@yahoo.com>
2021-05-18 00:12:45 +0200

    mocked test and against 2H Database

--- src/test/java/com/musala/gateways/controller/GatewaysControllerTestMocked.java
+++ src/test/java/com/musala/gateways/controller/GatewaysControllerTestMocked.java
@@ -1,42 +1,41 @@
 package com.musala.gateways.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musala.gateways.exception.BadRequestException;
+import com.musala.gateways.exception.NotFoundException;
 import com.musala.gateways.openapi.model.GatewayRequest;
 import com.musala.gateways.openapi.model.GatewayResponse;
 import com.musala.gateways.openapi.model.GatewayUpdateRequest;
-import com.musala.gateways.repository.GatewayRepository;
+import com.musala.gateways.service.GateWayService;
 import com.musala.gateways.utils.TestHelper;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.transaction.Transactional;
+import java.util.Collections;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@Transactional
-class GatewaysControllerTest {
+@WebMvcTest(GatewaysController.class)
+class GatewaysControllerTestMocked {
     private static final String GATEWAYS = "/gateways";
     private static final String GATEWAYS_SERIAL_NUMBER = "/gateways/{serialNumber}";
     @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    private GatewayRepository gatewayRepository;
+    private MockMvc mockMvc;
+    @MockBean
+    private GateWayService gateWayService;
 
     private TestHelper testHelper;
-
-    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -44,21 +43,11 @@ class GatewaysControllerTest {
     @BeforeEach
     public void setup() {
         testHelper = new TestHelper();
-        gatewayRepository.saveAll(testHelper.gatewayList());
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
-
     }
-
-    @AfterEach
-    public void destroy() {
-        gatewayRepository.deleteAll();
-    }
-
 
     @Test
     void getAllGateways() throws Exception {
+        when(gateWayService.getAllGateways()).thenReturn(testHelper.mappedGateways());
         mockMvc.perform(get(GATEWAYS)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -71,7 +60,7 @@ class GatewaysControllerTest {
 
     @Test
     void getAllGateways_emptyBody() throws Exception {
-        gatewayRepository.deleteAll();
+        when(gateWayService.getAllGateways()).thenReturn(Collections.emptyList());
         mockMvc.perform(get(GATEWAYS)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -83,15 +72,16 @@ class GatewaysControllerTest {
     void createGateway() throws Exception {
         GatewayRequest gatewayRequest = new GatewayRequest();
         gatewayRequest.setName("name1");
-        gatewayRequest.setSerialNumber("serial8");
+        gatewayRequest.setSerialNumber("serial1");
         gatewayRequest.setIpv4("192.168.1.2");
+        when(gateWayService.save(any())).thenReturn(testHelper.mappedGateways().get(0));
         mockMvc.perform(post(GATEWAYS)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(gatewayRequest))
         )
                 .andDo(print()).andExpect(status().isCreated())
-                .andExpect(jsonPath("$.serialNumber").value("serial8"))
+                .andExpect(jsonPath("$.serialNumber").value("serial1"))
                 .andExpect(jsonPath("$.name").value("name1"))
                 .andExpect(jsonPath("$.ipv4").value("192.168.1.2"));
 
@@ -140,6 +130,7 @@ class GatewaysControllerTest {
         gatewayRequest.setSerialNumber("serial1");
         gatewayRequest.setIpv4("254.168.1.2");
 
+        when(gateWayService.save(any())).thenThrow(new BadRequestException(String.format("gateway with serial number '%s' is already exists ", gatewayRequest.getSerialNumber())));
         mockMvc.perform(post(GATEWAYS)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -151,6 +142,7 @@ class GatewaysControllerTest {
     @Test
     void deleteGateway() throws Exception {
         String serial = "serial1";
+        doNothing().when(gateWayService).deleteBySerialNumber(serial);
         mockMvc.perform(delete(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON)
         )
@@ -160,6 +152,7 @@ class GatewaysControllerTest {
     @Test
     void deleteGateway_not_found() throws Exception {
         String serial = "serial8";
+        doThrow(NotFoundException.class).when(gateWayService).deleteBySerialNumber(serial);
         mockMvc.perform(delete(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isNotFound());
@@ -168,6 +161,7 @@ class GatewaysControllerTest {
     @Test
     void getGatewayBySerialNumber() throws Exception {
         String serial = "serial1";
+        when(gateWayService.getGatewayBySerialNumber(serial)).thenReturn(testHelper.mappedGateways().get(0));
         mockMvc.perform(get(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -181,6 +175,9 @@ class GatewaysControllerTest {
     @Test
     void getGatewayBySerialNumber_not_found() throws Exception {
         String serial = "serial8";
+        when(gateWayService.getGatewayBySerialNumber(serial)).thenThrow(new NotFoundException(String.format("gateway with serial number '%s' was not found", serial)));
+
+        doThrow(NotFoundException.class).when(gateWayService).deleteBySerialNumber(serial);
         mockMvc.perform(get(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().isNotFound());
@@ -198,6 +195,7 @@ class GatewaysControllerTest {
         response.setName("New Name");
         response.setIpv4("192.168.1.100");
 
+        when(gateWayService.update(serial, request)).thenReturn(response);
         mockMvc.perform(put(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -211,10 +209,11 @@ class GatewaysControllerTest {
 
     @Test
     void updateGateway_not_found() throws Exception {
-        String serial = "serial8";
+        String serial = "serial1";
         GatewayUpdateRequest request = new GatewayUpdateRequest();
         request.setName("New Name");
         request.setIpv4("192.168.1.100");
+        when(gateWayService.update(serial, request)).thenThrow(NotFoundException.class);
         mockMvc.perform(put(GATEWAYS_SERIAL_NUMBER, serial)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
